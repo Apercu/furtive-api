737271477aa87167c5ebbbe07c418213da793b84
Balthazar Gronon <bgronon@gmail.com>
2015-10-17 00:46:40 +0000

    feat(net): add bonjour for discovery

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import express from 'express';
 import compression from 'compression';
 import bodyParser from 'body-parser';
+import mdns from 'mdns';
 
 import routes from './routes';
 
@@ -18,4 +19,8 @@ routes(router);
 server.use('/api', router);
 
 server.listen(3000);
+
+const ad = mdns.createAdvertisement(mdns.tcp('http'), 3000);
+ad.start();
+
 process.stdout.write('[Server listenning on port 3000.]\n');
